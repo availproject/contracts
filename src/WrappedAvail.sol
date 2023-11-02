c9ffc6aae3e8ce699cfdaf0ae6b6449e13ba6b80
QEDK <1994constant@gmail.com>
2023-11-02 17:30:50 +0400

    feat: init WAVL

---
+++ src/WrappedAvail.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.22;
+
+import {ERC20, ERC20Permit} from "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ISuccinctBridge} from "./interfaces/ISuccinctBridge.sol";
+
+contract WrappedAvail is ERC20Permit {
+    ISuccinctBridge public bridge;
+
+    mapping(bytes32 => bool) isMinted;
+
+    error AlreadyMinted();
+    error InvalidProof();
+
+    constructor(ISuccinctBridge _bridge) ERC20Permit("Wrapped Avail") ERC20("WAVL", "Wrapped Avail") {
+        bridge = _bridge;
+    }
+
+    function mint(address destination, uint256 amount, uint256 depositId, bytes32[] calldata proof) external {
+        bytes32 depositHash = keccak256(abi.encodePacked(destination, amount, depositId));
+        if (isMinted[depositHash]) {
+            revert AlreadyMinted();
+        }
+        if (!bridge.verify(proof, depositHash)) {
+            revert InvalidProof();
+        }
+        isMinted[depositHash] = true;
+        _mint(destination, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
