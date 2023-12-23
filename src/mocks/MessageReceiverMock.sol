92ec80a743a74fcb6032bd38b932eb457d434dd9
qedk <1994constant@gmail.com>
2023-12-23 23:52:36 +0400

    test: add MessageReceiver tests

--- src/mocks/MessageReceiverMock.sol
+++ src/mocks/MessageReceiverMock.sol
@@ -4,9 +4,13 @@ pragma solidity ^0.8.23;
 import {MessageReceiver} from "src/MessageReceiver.sol";
 
 contract MessageReceiverMock is MessageReceiver {
+    event MessageReceived(bytes32 indexed from, bytes data);
+
     function initialize(address bridge) external initializer {
         __MessageReceiver_init(bridge);
     }
 
-    function _onAvailMessage(bytes32 from, bytes calldata data) internal virtual override {}
+    function _onAvailMessage(bytes32 from, bytes calldata data) internal virtual override {
+        emit MessageReceived(from, data);
+    }
 }
---
+++ test/MessageReceiverTest.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.23;
+
+import {MessageReceiver, MessageReceiverMock} from "src/mocks/MessageReceiverMock.sol";
+import {Vm, Test} from "forge-std/Test.sol";
+
+contract MessageReceiverTest is Test {
+    MessageReceiverMock public receiver;
+    address public bridge;
+
+    function setUp() external {
+        bridge = makeAddr("bridge");
+        receiver = new MessageReceiverMock();
+        receiver.initialize(bridge);
+    }
+
+    function testRevertOnlyBridge_onAvailMessage(address sender, bytes32 from, bytes calldata data) external {
+        vm.assume(sender != bridge);
+        vm.prank(sender);
+        vm.expectRevert(MessageReceiver.OnlyAvailBridge.selector);
+        receiver.onAvailMessage(from, data);
+    }
+
+    function test_onAvailMessage(bytes32 from, bytes calldata data) external {
+        vm.prank(bridge);
+        vm.expectEmit(true, false, false, true, address(receiver));
+        emit MessageReceiverMock.MessageReceived(from, data);
+        receiver.onAvailMessage(from, data);
+    }
+}
