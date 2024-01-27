14d14e9e2940ac47948161167849355a7149b78e
qedk <1994constant@gmail.com>
2024-01-27 17:37:15 +0400

    feat: move vars to env vars

--- script/DeployMock.s.sol
+++ script/DeployMock.s.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.23;
 import {TransparentUpgradeableProxy} from
     "lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {ProxyAdmin} from "lib/openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
-import {AvailBridge} from "src/AvailBridge.sol";
+import {DummyAvailBridge} from "src/mocks/DummyAvailBridge.sol";
 import {WrappedAvail} from "src/WrappedAvail.sol";
 import {IWrappedAvail} from "src/interfaces/IWrappedAvail.sol";
-import {IVectorx} from "src/interfaces/IVectorx.sol";
+import {IDummyVectorx} from "src/mocks/interfaces/IDummyVectorx.sol";
 import {Script} from "forge-std/Script.sol";
 
 contract GetProofMockScript is Script {
@@ -16,11 +16,11 @@ contract GetProofMockScript is Script {
         address admin = vm.envAddress("ADMIN");
         address vectorx = vm.envAddress("VECTORX");
         ProxyAdmin proxyAdmin = new ProxyAdmin(admin);
-        address impl = address(new AvailBridge());
-        AvailBridge bridge = AvailBridge(address(new TransparentUpgradeableProxy(impl, address(proxyAdmin), "")));
+        address impl = address(new DummyAvailBridge());
+        DummyAvailBridge bridge = DummyAvailBridge(address(new TransparentUpgradeableProxy(impl, address(proxyAdmin), "")));
         WrappedAvail avail = new WrappedAvail(address(bridge));
         bridge.initialize(
-            0, admin, IWrappedAvail(address(avail)), admin, admin, IVectorx(vectorx)
+            0, admin, IWrappedAvail(address(avail)), admin, admin, IDummyVectorx(vectorx)
         );
         vm.stopBroadcast();
     }
--- script/GetProofMockScript.s.sol
+++ script/GetProofMockScript.s.sol
@@ -5,10 +5,12 @@ import {TransparentUpgradeableProxy} from
     "lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {ProxyAdmin} from "lib/openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
 import {AvailBridge} from "src/AvailBridge.sol";
+import {IAvailBridge} from "src/interfaces/IAvailBridge.sol";
 import {ERC20Mock} from "src/mocks/ERC20Mock.sol";
 import {IWrappedAvail} from "src/interfaces/IWrappedAvail.sol";
 import {VectorxMock, IVectorx} from "src/mocks/VectorxMock.sol";
 import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
 
 contract GetProofMockScript is Script {
     function run() external {
@@ -21,6 +23,18 @@ contract GetProofMockScript is Script {
         bridge.initialize(0, msg.sender, IWrappedAvail(address(avail)), msg.sender, msg.sender, IVectorx(vectorx));
         avail.mint(msg.sender, 1 ether);
         bridge.sendAVAIL(bytes32(uint256(1)), 1 ether);
+        console.logBytes32(bridge.isSent(0));
+        IAvailBridge.Message memory message = IAvailBridge.Message(
+            0x02,
+            bytes32(bytes20(0x681257BED628425a28B469114Dc21A7c30205cFD)),
+            bytes32(uint256(1)),
+            2,
+            1,
+            abi.encode(bytes32(0), 1 ether),
+            0
+        );
+        console.logBytes(abi.encode(message));
+        console.logBytes32(keccak256(abi.encode(message)));
         vm.stopBroadcast();
     }
 }
