e94ea6b94fd5b91a91098c1dfdafc7daf081302b
qedk <1994constant@gmail.com>
2024-02-18 01:40:14 +0400

    feat: add deployment script, use cancun version

---
+++ script/DeployMessageReceiver.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.23;
+
+import {MessageReceiverMock} from "src/mocks/MessageReceiverMock.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract DeployMessageReceiver is Script {
+    function run() external {
+        vm.startBroadcast();
+        address bridge = vm.envAddress("BRIDGE");
+        MessageReceiverMock receiver = new MessageReceiverMock();
+        receiver.initialize(bridge);
+        vm.stopBroadcast();
+    }
+}
