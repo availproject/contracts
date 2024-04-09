406fad4a5fc2d5174b87986a66cd27c5e864c893
qedk <1994constant@gmail.com>
2024-04-09 20:54:42 +0400

    lint: run lint

--- test/AvailAttestationTest.t.sol
+++ test/AvailAttestationTest.t.sol
@@ -16,18 +16,14 @@ contract AvailAttestationTest is Test {
         vm.etch(bridge, "0xFF");
         attestation = new AvailAttestationMock();
         vm.mockCall(
-            address(bridge),
-            abi.encodeWithSelector(bytes4(keccak256("vectorx()"))),
-            abi.encode(address(vectorx))
+            address(bridge), abi.encodeWithSelector(bytes4(keccak256("vectorx()"))), abi.encode(address(vectorx))
         );
         attestation.initialize(IAvailBridge(bridge));
     }
 
     function testRevertInvalidAttestationProof_attest(IAvailBridge.MerkleProofInput calldata input) external {
         vm.mockCall(
-            address(bridge),
-            abi.encodeWithSelector(IAvailBridge.verifyBlobLeaf.selector, input),
-            abi.encode(false)
+            address(bridge), abi.encodeWithSelector(IAvailBridge.verifyBlobLeaf.selector, input), abi.encode(false)
         );
         vm.expectRevert(AvailAttestation.InvalidAttestationProof.selector);
         attestation.attest(input);
@@ -37,9 +33,7 @@ contract AvailAttestationTest is Test {
         vm.assume(input.dataRootIndex < type(uint24).max && input.leafIndex <= type(uint128).max);
         vectorx.setStartBlock(input.rangeHash, startBlockNumber);
         vm.mockCall(
-            address(bridge),
-            abi.encodeWithSelector(IAvailBridge.verifyBlobLeaf.selector, input),
-            abi.encode(true)
+            address(bridge), abi.encodeWithSelector(IAvailBridge.verifyBlobLeaf.selector, input), abi.encode(true)
         );
         attestation.attest(input);
         assertEq(vectorx.rangeStartBlocks(input.rangeHash), startBlockNumber);
